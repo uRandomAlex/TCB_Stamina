9a40f3edf174a8941cc31673076a5f3c14047531
TheCodingBeast <thecodingbeast@gmail.com>
2014-08-30 23:27:52 +0200

    License Fix
    Fixed License

--- tcb_stamina/sh_stamina.lua
+++ tcb_stamina/sh_stamina.lua
@@ -1,8 +1,8 @@
 /*---------------------------------------------------------------------------
 	
-	Made By: TheCodingBeast
+	Creator: TheCodingBeast - TheCodingBeast.com
 	This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
-	To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+	To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
 	
 ---------------------------------------------------------------------------*/
 
